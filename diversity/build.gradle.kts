db240c49ffeb837c228007e3c044724b710b229e
Fábián Edina <ayden.constantyne@gmail.com>
2024-09-23 17:45:26 +0200

    plugins stuck really

--- diversity/build.gradle.kts
+++ diversity/build.gradle.kts
@@ -2,43 +2,20 @@ plugins {
     id("hu.bme.mit.ase.shingler.gradle.java")
 }
 
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
-
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-val junitVersion = "5.10.0"
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
 
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    implementation(libs.slf4j.api)
+    implementation(libs.picocli)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    runtimeOnly(libs.slf4j.log4j.impl)
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation(libs.junit.jupiter.core)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
 
     api(project(":lib"))
     api(project(":logic"))
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
 }
 
 application {
